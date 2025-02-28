e2e5b2ae3a56a118b3fa5e0b2c189ae6a6c9dcc8
Hadi Mohagheghi <Hadimohagheghi@live.com>
2025-02-28 12:22:03 +0330

    تنظیمات اولیه برای Entity Properties

--- command/src/2.Infrastructures/Data/AcademyManagement.Infrastructures.Data.SqlServer/Student/StudentConfiguration.cs
+++ command/src/2.Infrastructures/Data/AcademyManagement.Infrastructures.Data.SqlServer/Student/StudentConfiguration.cs
@@ -14,7 +14,26 @@ namespace AcademyManagement.Infrastructures.Data.SqlServer.Student
         {
             builder.HasKey(p => p.Id);
             builder.Property(p => p.FirstName).IsRequired().HasMaxLength(100);
-            
+            builder.Property(p => p.Age)
+                .HasColumnType("decimal(18,2)");
+            builder.Property(c => c.CreateDate)
+                .HasDefaultValueSql("GETDATE()");
+            builder.Property(p => p.IsActive)
+                .HasDefaultValue(true);
+
+            //تنظیمات پایه ای
+            //مقادیر محاسباتی
+            builder.Property(p => p.Quantity)
+                .IsRequired();
+            builder.Property(p => p.TotalPrice)
+                .HasComputedColumnSql("[Quantity] * [Price]");
+            //تولید کننده مقدار
+            //Value Generator
+
+
+            //Concurrency
+            //shadow Property
+            //...
         }
     }
 }
